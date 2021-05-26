26a20db7cc77cd380846b72ff090909f964ccce5
Daniel Goude <daniel@goude.se>
2021-05-26 22:49:45 +0200

    update

---
+++ getaround.sh
@@ -0,0 +1,9 @@
+GETAROUND_ROOT="$HOME/getaround"
+
+# Define minimal aliases
+alias ls="ls --color"
+alias vim="vim -C $GETAROUND_ROOT/vimrc"
+
+function getaround {
+  echo "Welcome to getaround."
+}
