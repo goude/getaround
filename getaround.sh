9d9e1ebb6fb61f6a93abe0926e7f37c187f9ff35
Daniel Goude <daniel@goude.se>
2025-07-25 09:55:14 +0200

    tweaks

--- getaround.sh
+++ getaround.sh
@@ -1,60 +1,65 @@
+# Getaround shell setup
 GETAROUND_ROOT="$HOME/getaround"
 
-# aliases to protect against mistakes (force interactive mode)
+###--- PROTECTIVE ALIASES ---###
 alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -i'
 
-# basic command customization aliases
+###--- BASIC COMMAND ALIASES ---###
 alias df='df -h'
+alias ls='ls --color'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ta='tmux attach || tmux new'
 
-# git aliases
+###--- GIT ALIASES ---###
 alias gs='git status'
 alias ga='git add'
 alias gca='git commit -a'
 alias gp='git push'
 
-# directory navigation aliases
-alias ..='cd ..'
-alias ...='cd ../..'
+###--- SYSTEM SHORTCUTS ---###
+alias sau='sudo apt update && sudo apt upgrade -y'
 
-# misc aliases
-alias ls="ls --color"
-#alias vim="vim -u $GETAROUND_ROOT/vimrc"
-alias ta='tmux attach || tmux new'
+###--- SHELL BEHAVIOR ---###
+set -o vi                   # vi keybindings
+set bell-style none         # no bell sound
 
-# none, visible or audible
-set bell-style none
+# Show vi mode in prompt
+bind 'set show-mode-in-prompt on'
+bind 'set vi-ins-mode-string "\1\e[32m\2[INS]\1\e[0m\2 "'
+bind 'set vi-cmd-mode-string "\1\e[31m\2[NORMAL]\1\e[0m\2 "'
 
-alias sau="sudo apt update && sudo apt upgrade -y"
+# Optional Ctrl-F menu completion (Fish-like)
+bind '"\C-f": menu-complete'
 
-# disable/enable 8bit input
-#set meta-flag on
-#set input-meta on
-#set output-meta on
-#set convert-meta off
+# Clear screen in vi-insert mode with Ctrl-L
+bind -m vi-insert '"\C-l": clear-screen'
 
-# use vi key bindings for input
-# https://blog.bugsnag.com/tmux-and-vim/
-set editing-mode vi
-set keymap vi
+###--- ENHANCED HISTORY / SUGGESTIONS ---###
 
-# Fix Ctrl-L not clearing screen in Bash
-#set keymap vi-insert
-#$if Bash
-#"\C-l": "\e\C-la"
-#$endif
+# Fzf fuzzy search (Ctrl-R enhanced history)
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
-# help
+# Fish-style history suggestions
+[ -f ~/.bash-autosuggestions/bash-autosuggestions.sh ] && \
+  source ~/.bash-autosuggestions/bash-autosuggestions.sh
+
+###--- FUNCTIONS ---###
+
+# Welcome/help function
 function getaround {
   echo "Welcome to getaround."
 }
 
-# git quicksave: commit and push
+# Git quicksave: commit and push
 save() {
-  if [ -z "${1+x}" ]; then
+  if [ -z "$1" ]; then
     echo "Missing commit message."
   else
     git commit -a -m "$1" && git push
   fi
 }
+
+# --- END OF FILE ---
